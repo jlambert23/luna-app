019696f613b09dbef32752146cf1466b2b6a72b1
Jay <jb.lambert23@gmail.com>
2019-09-09 02:17:06 -0400

    added react-native-elements

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
+import { Button, colors, ThemeProvider } from 'react-native-elements';
+
+const theme = {
+  colors: {
+    ...Platform.select({
+      default: colors.platform.android,
+    }),
+  },
+  Button: {
+    containerStyle: {
+      marginTop: 80,
+      marginBottom: 10,
+    }
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <ThemeProvider theme = {theme}>
+      <Button title="Hello"/>
+      <Button title="World!" titleStyle={{ color: 'pink' }} />
+    </ThemeProvider>
   );
 }
 
