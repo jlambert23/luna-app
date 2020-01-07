9edde6affabe616a7a890bf9d174343df24a17ce
Jay <jb.lambert23@gmail.com>
2020-01-06 22:38:16 -0500

    Add glitch api.

--- mobile/config.js
+++ mobile/config.js
@@ -3,7 +3,7 @@ import Constants from 'expo-constants';
 const config = Constants.manifest.extra;
 
 export const
-  baseUrl = config.connectionString;
+  baseUrl = config.connectionStrings.glitch;
 
 export default {
   baseUrl,
