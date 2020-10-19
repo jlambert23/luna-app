b73998a0d04829054c3b515caadb3f5802a3a257
Jay <jb.lambert23@gmail.com>
2020-10-19 09:57:01 -0400

    [mobile] fix service error handling

--- mobile/services/pets/index.js
+++ mobile/services/pets/index.js
@@ -3,82 +3,54 @@ import { baseUrl } from '../../config';
 const api = `${baseUrl}/pet`;
 
 export const getPets = async () => {
-  try {
-    const response = await fetch(api, {
-      method: 'GET',
-      headers: { Accept: 'application/json' },
-    });
-    return await response.json();
-  } catch (error) {
-    return error;
-  }
+  const response = await fetch(api, {
+    method: 'GET',
+    headers: { Accept: 'application/json' },
+  });
+  return response.json();
 };
 
 export const getPetById = async id => {
-  try {
-    const response = await fetch(`${api}/${id}`, {
-      method: 'GET',
-      headers: { Accept: 'application/json' },
-    });
-    return await response.json();
-  } catch (error) {
-    return error;
-  }
+  const response = await fetch(`${api}/${id}`, {
+    method: 'GET',
+    headers: { Accept: 'application/json' },
+  });
+  return response.json();
 };
 
 export const createPet = async name => {
-  try {
-    const response = await fetch(api, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name }),
-    });
-
-    return await response.json();
-  } catch (error) {
-    return error;
-  }
+  const response = await fetch(api, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name }),
+  });
+  return response.json();
 };
 
 export const updatePet = async pet => {
-  try {
-    const response = await fetch(`${api}/${pet._id}`, {
-      method: 'PATCH',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(pet),
-    });
-
-    return await response.json();
-  } catch (error) {
-    return error;
-  }
+  const response = await fetch(`${api}/${pet._id}`, {
+    method: 'PATCH',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(pet),
+  });
+  return response.json();
 };
 
 export const deletePet = async id => {
-  try {
-    const response = await fetch(`${api}/${id}`, { method: 'DELETE' });
-
-    if (response.status !== 204) {
-      throw new Error(`Unexpected response code ${response.status}.`);
-    }
-
-    return await response.json();
-  } catch (error) {
-    return error;
+  const response = await fetch(`${api}/${id}`, { method: 'DELETE' });
+  if (response.status !== 204) {
+    throw new Error(`Unexpected response code ${response.status}.`);
   }
+  return response.json();
 };
 
 export const addNewPoop = async id => {
-  try {
-    const response = await fetch(`${api}/${id}/poops`, { method: 'POST' });
-    return await response.json();
-  } catch (error) {
-    return error;
-  }
+  const response = await fetch(`${api}/${id}/poops`, { method: 'POST' });
+  return response.json();
 };
