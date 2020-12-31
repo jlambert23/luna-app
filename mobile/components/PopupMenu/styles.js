b3d5758fb33409e26a9d79a54874917ba4ef829b
jlambert23 <jb.lambert23@gmail.com>
2020-12-31 03:38:57 +0000

    cd backend

--- mobile/components/PopupMenu/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-  icon: {
-    color: '#f4511e',
-    padding: 10,
-    fontSize: 20,
-  },
-});
--- mobile/config.js
+++
@@ -1,9 +0,0 @@
-import Constants from 'expo-constants';
-
-const config = Constants.manifest.extra;
-
-export const baseUrl = config.connectionStrings.rylo;
-
-export default {
-  baseUrl,
-};
